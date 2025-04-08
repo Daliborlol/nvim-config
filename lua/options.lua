f1ab803dfe9e4b07e7b669f4e456732c71de3d03
Daliborlol <englisch.dalibor@gmail.com>
2025-04-09 00:02:24 +0200

    actually im goated

--- lua/options.lua
+++ lua/options.lua
@@ -5,4 +5,18 @@ require "nvchad.options"
 local o = vim.o
 o.rnu = true
 
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
