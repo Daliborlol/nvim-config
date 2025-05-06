63518215132679713948d52396a33c3fcce991e6
Daliborlol <englisch.dalibor@gmail.com>
2025-05-06 09:28:56 +0200

    update because i have an exam

--- lua/plugins/augment.lua
+++ lua/plugins/augment.lua
@@ -1,8 +1,9 @@
-return
-  {
-    "augmentcode/augment.vim",
-    lazy=false,
-    init = function()
-      vim.g.augment_workspace_folder = {'/home/dalibor/programming/apertia/monitoring/main'}
-    end
-  }
+return {}
+-- return
+--   {
+--     "augmentcode/augment.vim",
+--     lazy=false,
+--     init = function()
+--       vim.g.augment_workspace_folder = {'/home/dalibor/programming/apertia/monitoring/main'}
+--     end
+--   }
